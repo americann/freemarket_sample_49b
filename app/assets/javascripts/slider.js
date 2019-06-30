92faa36e81fa03089b039a1192ccee6eb6ff8667
696b <k.kaitophone@gmail.com>
2019-06-30 21:01:57 +0900

    カルーセルのボタン、ドットの編集

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,6 +1,9 @@
 $(document).on("turbolinks:load", function(){
   $(function() {
-    $('.slider').slick();
-    
+    $('.slider').slick({
+      prevArrow: '<img src="assets/slider/carousel-prev.png" class="slide-arrow prev-arrow">',
+      nextArrow: '<img src="assets/slider/carousel-next.png" class="slide-arrow next-arrow">',
+      dots: true
+    });
   });
 }); 
