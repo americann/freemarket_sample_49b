244b5a7c0650dc433c8ec8025e71533eea6203fb
696b <k.kaitophone@gmail.com>
2019-07-02 20:46:41 +0900

    imgのsrcの変更

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -3,8 +3,8 @@ $(document).on("turbolinks:load", function(){
     $('.slider').slick({
       autoplay: true,
       autoplaySpeed: 5000,
-      prevArrow: '<img src="assets/slider/carousel-prev.png" class="slide-arrow prev-arrow">',
-      nextArrow: '<img src="assets/slider/carousel-next.png" class="slide-arrow next-arrow">',
+      prevArrow: '<img src="/carousel-prev.png" class="slide-arrow prev-arrow">',
+      nextArrow: '<img src="/carousel-next.png" class="slide-arrow next-arrow">',
       dots: true
     });
   });
