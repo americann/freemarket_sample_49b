fda2fcebb0ad05f1a401a8c2775a6d6a6da876e6
696b <k.kaitophone@gmail.com>
2019-07-06 12:11:50 +0900

    update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,17 @@
+# # require 'rails_helper'
+
+# # RSpec.describe User, type: :model do
+# #   pending "add some examples to (or delete) #{__FILE__}"
+# # end
+
+
 require 'rails_helper'
 describe User do 
   describe '#create' do
     it "is invalid without a email" do
-      user = User.new(email: "sample@gmail.com", password: "samplesample", password_confimation: "samplesample")
+      user =  User.new(email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      # expect(user.errors[:email]).to include("can't be blank")
     end
   end
 end
