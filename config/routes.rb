b7abdcb34c079c63d8cd2c1bf4f05dc38a88298c
696b <k.kaitophone@gmail.com>
2019-07-06 15:51:02 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  get   'users'   =>  'users#show'
+  resources  :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
