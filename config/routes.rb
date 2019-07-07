7d9a68d0dcff20e99a1bcfa0e8796e05d6627209
kj-takeda <tktk19831025@gmail.com>
2019-07-07 16:17:42 +0900

    rere
    コンフリクト修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'products#index'
   get  'users'   =>  'users#show'
   get  'exhibit_new' =>  'products#exhibit_new'
-
+  resources  :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
