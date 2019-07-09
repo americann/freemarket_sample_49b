31e1f8a5893585b9fee82eaadc4e168986fba35c
Hiroshi <ionazun0805@gmail.com>
2019-07-09 20:34:09 +0900

    Update header.js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,10 +1,27 @@
 $(function(){
+
+  var category_ul = ".header-nav-left__nav1 ul"
+  var brand_ul = ".header-nav-left__nav2 ul"
   
   $(".navbar").on("mouseover", function(){
     $(this.querySelector("span")).css({"color": "#0099e8"});
-  }).on("mouseout",function(){
+  }).on("mouseout", function(){
     $(this).children().css({"color": ""});
   })
 
-  
+  $("#category").on("mouseover", function(){
+    $(category_ul).show();
+  }).on("mouseout", function(){
+    $(category_ul).hide();
+  })
+
+
+
+  $("#brand").on("mouseover", function(){
+    $(brand_ul).show();
+  }).on("mouseout", function(){
+    $(brand_ul).hide();
+  })
+
+
 })
