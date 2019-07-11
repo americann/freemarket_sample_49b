d6f303c9d29556b9f8510726ff51861c5a7b0dd4
Hiroshi <ionazun0805@gmail.com>
2019-07-11 19:22:40 +0900

    Update header.js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,27 +1,68 @@
 $(function(){
 
-  var category_ul = ".header-nav-left__nav1 ul"
-  var brand_ul = ".header-nav-left__nav2 ul"
-  
+  var parent = ".category"
+  var child = ".category__child"
+  var parent_with_child = ".category, .category__child"
+  var child_li = ".category__child li"
+  var grandchild = ".category__grandchild"
+  var grandchild_li = ".category__grandchild li"
+  var GGC = ".category__GGC"
+  var GC_with_GGC = ".category__grandchild, .category__GGC"
+  var GC_li_with_GGC_li = ".category__grandchild li, .category__GGC li"
+
+
   $(".navbar").on("mouseover", function(){
     $(this.querySelector("span")).css({"color": "#0099e8"});
   }).on("mouseout", function(){
     $(this).children().css({"color": ""});
   })
-
+  
   $("#category").on("mouseover", function(){
-    $(category_ul).show();
+    $(parent_with_child).show();
   }).on("mouseout", function(){
-    $(category_ul).hide();
+    $(parent).hide();
+  })
+  
+  $(parent_with_child).on("mouseover", function(){
+    $(grandchild).show();
+  }).on("mouseout", function(){
+    $(grandchild).hide();
+  })
+  $(GC_with_GGC).on("mouseover", function(){
+    $(GGC).show();
+  }).on("mouseout", function(){
+    $(GGC).hide();
   })
 
-
+  $(child_li).on("mouseover", function(){
+    $(this).css({"background-color": "#EA342D"});
+    $(this).children().css({"color": "#fff"});
+  }).on("mouseout", function(){
+    $(child_li).css({"background-color": ""});
+    $(this).children().css({"color": ""});
+  })
+  
+  $(GC_li_with_GGC_li).on("mouseover", function(){
+    $(this).css({"background-color": "#f5f5f5"});
+    $(this).children().css({"color": ""});
+  }).on("mouseout", function(){
+    $(GC_li_with_GGC_li).css({"background-color": ""});
+    // $(this).children().css({"color": ""});
+  })
 
   $("#brand").on("mouseover", function(){
-    $(brand_ul).show();
+    $(this).children("ul").show();
   }).on("mouseout", function(){
-    $(brand_ul).hide();
+    $(this).children("ul").hide();
   })
 
-
+  $("#brand li").on("mouseover", function(){
+    $(this).css({"background-color": "#EA342D"});
+    $(this).children().css({"color": "#fff"});
+  }).on("mouseout", function(){
+    $("#brand li").css({"background-color": ""});
+    $(this).children().css({"color": ""});
+  })
+  
+  
 })
