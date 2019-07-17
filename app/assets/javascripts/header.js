6666b162a7d8155415203f4b4fb94e7c29c2af34
Hiroshi <ionazun0805@gmail.com>
2019-07-17 21:39:57 +0900

    Update header.js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,68 +1,67 @@
 $(function(){
 
-  var parent = ".category"
-  var child = ".category__child"
-  var parent_with_child = ".category, .category__child"
-  var child_li = ".category__child li"
-  var grandchild = ".category__grandchild"
-  var grandchild_li = ".category__grandchild li"
-  var GGC = ".category__GGC"
-  var GC_with_GGC = ".category__grandchild, .category__GGC"
-  var GC_li_with_GGC_li = ".category__grandchild li, .category__GGC li"
+  var icon = ".far.fa-heart, .fa.fa-bell, .fa.fa-check, .fa.fa-smile"
 
+  $(".category__child").hide();
 
   $(".navbar").on("mouseover", function(){
     $(this.querySelector("span")).css({"color": "#0099e8"});
+    $(this.querySelector(icon)).css({"color": "#0099e8"});
   }).on("mouseout", function(){
     $(this).children().css({"color": ""});
   })
   
-  $("#category").on("mouseover", function(){
-    $(parent_with_child).show();
-  }).on("mouseout", function(){
-    $(parent).hide();
-  })
-  
-  $(parent_with_child).on("mouseover", function(){
-    $(grandchild).show();
-  }).on("mouseout", function(){
-    $(grandchild).hide();
-  })
-  $(GC_with_GGC).on("mouseover", function(){
-    $(GGC).show();
-  }).on("mouseout", function(){
-    $(GGC).hide();
+  // $(".header-nav-left li").on("mouseover", function(){
+  //   $('ul', this).show();
+  //   $('ul li ul', this).hide();
+  // }).on("mouseout", function(){
+  //   $('ul', this).hide();
+  // })
+
+  $(".header-nav-left li").hover(function(){
+    $('ul', this).show();
+    $('ul li ul', this).hide();
+  },function(){
+    $('ul', this).hide();
   })
 
-  $(child_li).on("mouseover", function(){
+  $(".category__child li, .header-nav-left__nav2 ul li").on("mouseover", function(){
     $(this).css({"background-color": "#EA342D"});
     $(this).children().css({"color": "#fff"});
   }).on("mouseout", function(){
-    $(child_li).css({"background-color": ""});
+    $(this).css({"background-color": ""});
     $(this).children().css({"color": ""});
   })
   
-  $(GC_li_with_GGC_li).on("mouseover", function(){
+  $(".category__grandchild li").on("mouseover", function(){
     $(this).css({"background-color": "#f5f5f5"});
     $(this).children().css({"color": ""});
   }).on("mouseout", function(){
-    $(GC_li_with_GGC_li).css({"background-color": ""});
-    // $(this).children().css({"color": ""});
-  })
-
-  $("#brand").on("mouseover", function(){
-    $(this).children("ul").show();
-  }).on("mouseout", function(){
-    $(this).children("ul").hide();
-  })
-
-  $("#brand li").on("mouseover", function(){
-    $(this).css({"background-color": "#EA342D"});
-    $(this).children().css({"color": "#fff"});
-  }).on("mouseout", function(){
-    $("#brand li").css({"background-color": ""});
-    $(this).children().css({"color": ""});
+    $(this).css({"background-color": ""});
   })
   
-  
+  $("#notification").on("mouseover", function(){
+    $(".notification-box").show();
+  }).on("mouseout", function(){
+    $(".notification-box").hide();
+  })
+
+  $(".notification-box li").on("mouseover", function(){
+    $(this).css({"background-color": "#f5f5f5"});
+  }).on("mouseout", function(){
+    $(this).css({"background-color": ""});
+  })
+
+  $("#mypage").on("mouseover", function(){
+    $(".mypage-box").show();
+  }).on("mouseout", function(){
+    $(".mypage-box").hide();
+  })
+
+  $(".mypage-box li").on("mouseover", function(){
+    $(this).css({"background-color": "#f5f5f5"});
+  }).on("mouseout", function(){
+    $(this).css({"background-color": ""});
+  })
+
 })
