9bff5092d8c6d81f8497c2fc30df88c259e2a86e
nakanoyuki <yukich41@gmail.com>
2019-07-19 22:09:11 +0900

    productsテーブル修正

--- db/migrate/20190719122109_create_products.rb
+++ db/migrate/20190719122109_create_products.rb
@@ -2,14 +2,15 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string          :name
-      t.description     :text
-      t.state           :string
-      t.postage         :string
-      t.shopping_date   :string
-      t.price           :interger
-      t.size            :string
+      t.text            :description
+      t.string          :state
+      t.string          :postage
+      t.string          :shipping_date
+      t.integer         :price
+      t.string          :size
       t.references :user, foreign_key: true
       t.references :prefecture, foreign_key: true
+      t.references :category, foreign_key: true
       t.timestamps
     end
   end
