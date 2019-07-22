456557e1dbe06606a103ebb7fdfced00fe3e40f8
nakanoyuki <yukich41@gmail.com>
2019-07-22 13:59:23 +0900

    ユーザー新規登録画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:lastname,:firstname_kana,:lastname_kana,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:firstname,:lastname,:firstname_kana,:lastname_kana,:birthday,:zip_code])
   end
 
   private
