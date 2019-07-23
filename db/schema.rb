ff4311d5ff82eb8efd1bb148e9bf419b9c74347b
696b <k.kaitophone@gmail.com>
2019-07-23 21:11:39 +0900

    shipping_methodカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_092337) do
+ActiveRecord::Schema.define(version: 2019_07_23_113715) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_07_21_092337) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "shipping_method"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["prefecture_id"], name: "index_products_on_prefecture_id"
     t.index ["user_id"], name: "index_products_on_user_id"
