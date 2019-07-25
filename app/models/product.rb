01666e6d910835c8e53dac48969721943b3a2fc2
696b <k.kaitophone@gmail.com>
2019-07-25 19:52:21 +0900

    Revert "productとcateroryをアソシエーション"
    This reverts commit 66de8c7b35b5cdebb9550f5fc1f9e48066438cc8.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,4 @@ class Product < ApplicationRecord
     belongs_to :user
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to_active_hash :prefecture
-    belongs_to :category
 end
