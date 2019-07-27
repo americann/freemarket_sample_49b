6f28f323adb47008c672305c24c16544a8131301
nakanoyuki <yukich41@gmail.com>
2019-07-27 18:50:13 +0900

    モデルにバリデーションを追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,13 @@
 class Address < ApplicationRecord
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  
   belongs_to :user
+
+  validates :zip_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :block, presence: true
+
 end
