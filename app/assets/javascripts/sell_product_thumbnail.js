7a09613a9d9c77982405903177a1c30e4dc4dc7c
696b <k.kaitophone@gmail.com>
2019-08-01 11:56:02 +0900

    画像1枚までのサムネイル表示

--- app/assets/javascripts/sell_product_thumbnail.js
+++ app/assets/javascripts/sell_product_thumbnail.js
@@ -1,15 +1,20 @@
 $(document).on("turbolinks:load", function(){
-  function imageBox(file){
-    var image = ( message.image ) ? `<img class= "lower-message__image" src=${file} >` : "";
-    var html = `<div`
+  function buildHTML(file){
+    var html = `<li class="image-box">
+                  <img id="img"></img>
+                  <div class="button">
+                    <a class="edit">編集</a>
+                    <a class="delete">削除</a>
+                  </div>
+                </li>`
     return html;
   }
   $(function() {
     // アップロードするファイルを選択
-    $('input[type=file]').change(function() {
+    $('.hidden').change(function(){
       // 画像データの取得
       var file = $(this).prop('files')[0];
-  
+      console.log(file);
       // 画像以外は処理を停止
       if (! file.type.match('image.*')) {
         // クリア
@@ -18,8 +23,16 @@ $(document).on("turbolinks:load", function(){
         return;
       }
       // 画像表示
-      
+      $(".image-container").append(buildHTML(file));
 
+      // 仮
+      $('.hidden').after('<span></span>');
+      var reader = new FileReader();
+    reader.onload = function() {
+      $('#img').attr('src', reader.result);
+      $('span').html(img_src);
+    }
+    reader.readAsDataURL(file);
     });
   });
 }); 
