b0a7b1c4f389cf8d0fde64499a91b6ada52c5369
nakanoyuki <yukich41@gmail.com>
2019-08-07 21:45:11 +0900

    フォームの下にそれぞれのバリデーションをかける

---
+++ app/assets/javascripts/registration.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  $('#address_table').toggle();
+
+	$("#button").click(function() {
+
+    $("#address_table").toggle();
+    $("#button").toggle();
+    $("#user_table").toggle();
+
+  })
+
+});
+
+
