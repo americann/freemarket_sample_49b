9b36fcad5940234fc424a04601b3d6d278e5440a
kj-takeda <tktk19831025@gmail.com>
2019-08-10 18:36:52 +0900

    cardコントローラーにrespond_to do formatを記入

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,12 +9,19 @@ def new
 end
 
 def create
+  respond_to do |format|
  @card = Card.new(card_number:card_params[:card_number] , expiration_date:card_params[:expiration_date] , user_id: current_user.id)
   if @card.save
-    redirect_to index: :cards
+   format.html {redirect_to index: :cards}
   else
     flash.now[:alert] = '正しく情報を入力して下さい'
-    render :new
+    format.html {render :new}
+  end
+  format.json {
+    require 'payjp'
+    Payjp.api_key = "（pk_test_d7a950435be908a5fbe46bd8)"
+    current_user.card.update(token: params[:token])
+  }
   end
 end
 
