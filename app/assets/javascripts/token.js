c6f3c3135debdc62ce62889bb3320b8482f9d00a
kj-takeda <tktk19831025@gmail.com>
2019-08-11 20:04:53 +0900

    routesにcardsコントローラ#payjpアクションを作成　コントローラにpayjpアクションを定義　crad_new作成ページのform_forをform_withに変更

--- app/assets/javascripts/token.js
+++ app/assets/javascripts/token.js
@@ -1,16 +1,15 @@
 $(document).on("turbolinks:load", function(){
 $(function(){
-$(document).on('click','card-new__inside__security__cord__signup__btn', function(e) {
+$(".card-new__inside__security__cord__signup__btn").on('click', function(e) {
   e.preventDefault();
 
-  Payjp.setPublicKey('（pk_test_d7a950435be908a5fbe46bd8）'); //payjp API テスト秘密鍵を入力
+  Payjp.setPublicKey('pk_test_d7a950435be908a5fbe46bd8'); //payjp API テスト秘密鍵を入力
   var card = {
     number: parseInt($(".card-new-form__inside__card-num__text").val()), //カード番号入力欄
     cvc: parseInt($(".card-new__inside__security__cord__text").val()),　//セキュリティーコード入力欄のIDやクラス名
     exp_month: parseInt($(".card-new-form__inside__expiration-date__text__select1").val()), //有効月入力欄のIDやクラス名
     exp_year: parseInt($(".card-new-form__inside__expiration-date__text__select2").val())　//有効年入力欄のIDやクラス名
   };
-console.log(card);
 
   Payjp.createToken(card, function(status, response) {
     if (status == 200) {
@@ -22,7 +21,7 @@ console.log(card);
         dataType: 'json',
       })
       .done(function(){
-        //非同期通信失敗時の処理
+        //非同期通信成功時の処理
       })
       .fail(function(){
         //非同期通信失敗時の処理
