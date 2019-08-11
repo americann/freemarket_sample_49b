ace1efce25dc138eb04eff3b7405ad59a2393402
nakanoyuki <yukich41@gmail.com>
2019-08-11 14:22:04 +0900

    ステップバー作成

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,6 +1,8 @@
+$(document).on("turbolinks:load", function(){
 $(function() {
 
   $('#address_table').toggle();
+  $('#step2').toggle();
 
 	$("#button").click(function() {
 
@@ -8,8 +10,8 @@ $(function() {
     $("#button").toggle();
     $("#user_table").toggle();
 
+    $('#step2').toggle();
+    $('#step1').toggle();
   })
-
 });
-
-
+});
