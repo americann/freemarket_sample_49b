538c83743e15a2f8dfc7f01d1dd824f60f6594a0
kj-takeda <tktk19831025@gmail.com>
2019-08-11 15:17:59 +0900

    ターボリンクスを無効化するコードをtoken.jsに追加

--- app/assets/javascripts/token.js
+++ app/assets/javascripts/token.js
@@ -1,4 +1,4 @@
-
+$(document).on("turbolinks:load", function(){
 $(function(){
 $(document).on('click','card-new__inside__security__cord__signup__btn', function(e) {
   e.preventDefault();
@@ -10,6 +10,7 @@ $(document).on('click','card-new__inside__security__cord__signup__btn', function
     exp_month: parseInt($(".card-new-form__inside__expiration-date__text__select1").val()), //有効月入力欄のIDやクラス名
     exp_year: parseInt($(".card-new-form__inside__expiration-date__text__select2").val())　//有効年入力欄のIDやクラス名
   };
+console.log(card);
 
   Payjp.createToken(card, function(status, response) {
     if (status == 200) {
@@ -33,3 +34,4 @@ $(document).on('click','card-new__inside__security__cord__signup__btn', function
   });
 })
 })
+});
