1a5fbd748bc1f15f7ce6f9310f392f2a64488c20
kkazum <nov28kkazum@gmail.com>
2019-08-15 17:50:53 +0900

    create category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,9 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.where(parent_id: nil)
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = Product.where(category_id: @category.id)
+  end
 end
