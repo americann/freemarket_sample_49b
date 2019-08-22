7c1f2dd8e85e4f282f4cc9e930bf4b5ec618ae06
kj-takeda <tktk19831025@gmail.com>
2019-08-22 12:01:58 +0900

    jsでモーデルウィンドウの表示/非表示を実装　モーダルウィンドウの表示と共に背景がダークになりダークな背景をクリックするとモーダルも閉じる

--- app/assets/javascripts/modal_delete.js
+++ app/assets/javascripts/modal_delete.js
@@ -1,3 +1,15 @@
+$(document).on('turbolinks:load', function() {
 $(function(){
-  setTimeout("$('.notice').fadeOut('slow')", 1000) 
-  })
+  $('.detail-wrapper__btns__ele').on('click' , function(){
+    $('.modal_delete_js').show();
+  });
+  $('.modal_cancel').on('click',function(){
+    $('.modal_delete_js').hide();
+  });
+  $('.modal_delete_btn_js').on('click' , function(){
+    $('.modal_delete_js').hide();
+  });
+
+});
+setTimeout("$('.notice').fadeOut('slow')", 1000) ;
+});
