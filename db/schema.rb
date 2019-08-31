d46e46f5c834c9ce99673d10a0974ca71cf8cdce
nakanoyuki <yukich41@gmail.com>
2019-08-31 15:07:39 +0900

    edit migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_082516) do
+ActiveRecord::Schema.define(version: 2019_08_31_025245) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -104,7 +104,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_082516) do
     t.string "firstname"
     t.string "lastname"
     t.string "firstname_kana"
-    t.string "lastname_kana", default: ""
+    t.string "lastname_kana"
     t.date "birthday"
     t.string "uid"
     t.string "provider"
